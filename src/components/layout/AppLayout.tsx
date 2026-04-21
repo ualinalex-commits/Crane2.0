dcdd8fb578a41ff4d9e9e22fa5ef67d3d57fa62e
Alin Ungureanu <alin.watsel@yahoo.com>
2026-04-21 23:16:34 +0100

    Remove duplicate JibHeader import

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link, Outlet, useLocation } from 'react-router-dom';
 import { CraneMastBackground } from './CraneMastBackground';
 import { JibHeader } from './JibHeader';
-import { JibHeader } from './JibHeader';
 import { useAuth } from '@/contexts/AuthContext';
 import { useTestMode, useEffectiveProfile } from '@/contexts/TestModeContext';
 import {
